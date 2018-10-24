368ba4605ad724358fe25e930d100a23b770c2fc
matheusroleal <matheusroleal@gmail.com>
2018-10-24 08:44:31 -0300

    #3 primeira versão da estrutura de dados do jogo

---
+++ Ludo/src/lista/No.java
@@ -0,0 +1,20 @@
+package lista;
+
+public class No {
+	private Object elem;
+	private No prox;
+	
+	public No(Object e,No p) {
+		elem=e;
+		prox=p;
+	}
+	public Object getElem() {
+		return elem;
+	}
+	public No getProx() {
+		return prox;
+	}
+	public void setProx(No o) {
+		prox = o;
+	}
+}
